bdddcaa380450b4e4f1268aaffd35741f42879f0
Gheorghe Galan <ghitha87@gmail.com>
2018-08-08 19:22:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ if (name === "Susan") {
   console.log("The name is correct")
 } else {
   console.log("Expected the name to be Susan")
-} if console.log("The name is correct")
-} if console.log("The height is correct")
+} if match.{B}("The name is correct")
+} if match.{B}("The height is correct")
 }
 // Don't worry about this
 module.exports = { name, height
